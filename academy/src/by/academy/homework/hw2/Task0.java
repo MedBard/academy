cc024b48e3c46434236fa6fbc261e454cc4f4984
Ihar <rapsodus@mail.ru>
2021-01-31 00:36:55 +0300

    Task0 Correcting

--- academy/src/by/academy/homework/hw2/Task0.java
+++ academy/src/by/academy/homework/hw2/Task0.java
@@ -1,11 +1,10 @@
 package by.academy.homework.hw2;
 
 import java.util.Scanner;
-import java.util.Random;
 
 public class Task0 {
+	private static final Scanner scanner = new Scanner(System.in);
 
-	// Complete the pairs function below.
 	static int pairs(int k, int[] arr) {
 		int pairsCount = 0;
 		for (int i = 0; i < arr.length; i++) {
@@ -18,19 +17,15 @@ public class Task0 {
 		return pairsCount;
 	}
 
-	private static final Scanner scanner = new Scanner(System.in);
-
 	public static void main(String[] args) {
 		String[] nk = scanner.nextLine().split(" ");
 		int n = Integer.parseInt(nk[0]);
 		int k = Integer.parseInt(nk[1]);
 		int[] arr = new int[n];
-
 		String[] arrItems = scanner.nextLine().split(" ");
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(arrItems[i]);
 		}
-
 		int result = pairs(k, arr);
 		System.out.println(result);
 		scanner.close();
